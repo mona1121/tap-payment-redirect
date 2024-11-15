93c10030601c547fce66fb537f047f965b14be2f
mona1121 <134429123+mona1121@users.noreply.github.com>
2024-11-15 03:05:32 +0300

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,11 +1,16 @@
 export default function handler(req, res) {
-    const status = req.query.status;
+  if (req.method === 'GET') {
+    const { status, message } = req.query;
+    // Handle the status (success or failure) here
     
-    // Redirect to your Flutter app's page based on payment status
-    if (status === 'CAPTURED') {
-      res.redirect('payready://payment-success');
+    if (status === 'success') {
+      // Payment was successful
+      res.status(200).send('Payment Success! Thank you for your purchase.');
     } else {
-      res.redirect('payready://payment-failed');
+      // Payment failed
+      res.status(400).send('Payment Failed. Please try again.');
     }
+  } else {
+    res.status(404).send('Not Found');
   }
-  
+}
